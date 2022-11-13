91d1279950efc600fa9cd859cffd55c8482546cb
Ruslan Negoda <iarjuna108@gmail.com>
2022-11-13 17:35:42 +0200

    app complited

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import fetchPictures from './Api/Api';
@@ -9,113 +9,94 @@ import { Spiner } from './Loader/Loader';
 import Modal from './Modal/Modal';
 import { Searchbar } from './Searchbar/Searchbar';
 
-export class App extends Component {
-  state = {
-    search: '',
-    page: 1,
-    gallery: [],
-    isLoading: false,
-    error: null,
-    largeImage: '',
-    showModal: false,
-    showLoadMore: false,
-  };
+export const App = () => {
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [gallery, setGallery] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [largeImage, setLargeImage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [showLoadMore, setShowLoadMore] = useState(false);
 
-  componentDidUpdate(_, prevState) {
-    const prevSearch = prevState.search;
-    const prevPage = prevState.page;
+  useEffect(() => {
+    try {
+      setIsLoading(true);
 
-    const { search, page } = this.state;
-
-    // перевірка чи змінився пошук і чи змінилась сторінка, якщо щось змінилось фетчим
-    if (prevSearch !== search || prevPage !== page) {
-      try {
-        this.setState({ isLoading: true });
-
-        const data = fetchPictures(search, page);
-        data.then(data => {
-          // Перевіряємо чи щось знайшли, якщо ні то повідомляємо
-          if (data.hits.length === 0) {
-            toast.error('Nothing is found !', {
-              position: toast.POSITION.TOP_LEFT,
-            });
-          }
-
-          // Логіка відображення кнопки "Завантажити ще.."
-          const show = Math.ceil(data.total / 12);
-          this.setState({ showLoadMore: page < show });
-
-          // Cетим в стейт галерею, попередню плюс нову
-          this.setState(prevState => {
-            return { gallery: prevState.gallery.concat(data.hits) };
-          });
-        });
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ isLoading: false });
+      if (!search) {
+        return;
       }
+
+      const data = fetchPictures(search, page);
+      data.then(data => {
+        // Перевіряємо чи щось знайшли, якщо ні то повідомляємо
+        if (data.hits.length === 0) {
+          toast.error('Nothing is found !', {
+            position: toast.POSITION.TOP_LEFT,
+          });
+        }
+
+        // Логіка відображення кнопки "Завантажити ще.."
+        const show = Math.ceil(data.total / 12);
+
+        setShowLoadMore(page < show);
+
+        // Cетим в стейт галерею, попередню плюс нову
+        setGallery(state => {
+          return state.concat(data.hits);
+        });
+      });
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
     }
-  }
+  }, [page, search]);
 
   // Ф-ція на кнопка дозавантаження картинок
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMore = () => {
+    setPage(state => state + 1);
   };
 
   // отримуємо значення з інпут форми пошуку
-  onSearchValue = value => {
+  const onSearchValue = value => {
     if (value.value === '') {
       return toast.warn('Enter a word for search');
-    } else if (value.value === this.state.search) {
+    } else if (value.value === search) {
       return;
     }
 
-    this.setState({
-      search: value.value,
-      page: 1,
-      gallery: [],
-    });
+    setSearch(value.value);
+    setPage(1);
+    setGallery([]);
   };
 
   // Modall
-  openModal = largeImageURL => {
-    this.setState({
-      showModal: true,
-      largeImage: largeImageURL,
-    });
+  const openModal = largeImageURL => {
+    setShowModal(true);
+    setLargeImage(largeImageURL);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(state => !state);
   };
 
-  render() {
-    const { toggleModal, openModal } = this;
-    const { gallery, isLoading, showModal, largeImage, showLoadMore } =
-      this.state;
-    return (
-      <>
-        <Div>
-          <Searchbar onSubmit={this.onSearchValue} />
+  return (
+    <>
+      <Div>
+        <Searchbar onSubmit={onSearchValue} />
 
-          <ImageGallery gallery={gallery} openModal={openModal} />
-          {showModal && (
-            <Modal toggleModal={toggleModal} largeImage={largeImage} />
-          )}
-          <ToastContainer
-            autoClose={2500}
-            position="top-left"
-            theme="colored"
-          />
-          <div>
-            {isLoading && <Spiner />}
-            {showLoadMore && <LoadeMore loadMore={this.loadMore} />}
-          </div>
-        </Div>
-      </>
-    );
-  }
-}
+        {gallery && <ImageGallery gallery={gallery} openModal={openModal} />}
+        {showModal && (
+          <Modal toggleModal={toggleModal} largeImage={largeImage} />
+        )}
+        <ToastContainer autoClose={2500} position="top-left" theme="colored" />
+        <div>
+          {isLoading && <Spiner />}
+          {showLoadMore && <LoadeMore loadMore={loadMore} />}
+        </div>
+      </Div>
+    </>
+  );
+  // }
+};
